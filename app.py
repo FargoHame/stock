a4327274406b2a9e0570f09a76a479cc883042b2
braxtonova <85130590+braxtonova@users.noreply.github.com>
2021-06-18 12:45:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -115,66 +115,70 @@ def main():
 main()
 
 #part2
-
-st.header('Stock prediction')
-
-START = "2010-01-01"
-TODAY = date.today().strftime("%Y-%m-%d")
+def predict(): 
 
 
-stocks = ('AAPL', 'GOOG', 'MSFT', 'GME')
-selected_stock = st.selectbox('Select company for prediction', stocks)
-
-n_years = st.slider('Years of prediction:', 1, 15)
-period = n_years * 365
 
 
-@st.cache
-def load_data(ticker):
-    data = yf.download(ticker, START, TODAY)
-    data.reset_index(inplace=True)
-    return data
+	st.header('Stock prediction')
+
+	START = "2010-01-01"
+	TODAY = date.today().strftime("%Y-%m-%d")
+
+
+	stocks = ('AAPL', 'GOOG', 'MSFT', 'GME')
+	selected_stock = st.selectbox('Select company for prediction', stocks)
+
+	n_years = st.slider('Years of prediction:', 1, 15)
+	period = n_years * 365
+
+
+	@st.cache
+	def load_data(ticker):
+    	data = yf.download(ticker, START, TODAY)
+    	data.reset_index(inplace=True)
+    	return data
 
 	
-#data_load_state = st.text('Loading data...')
-data = load_data(selected_stock)
-#data_load_state.text('Loading data... done!')
+	#data_load_state = st.text('Loading data...')
+	data = load_data(selected_stock)
+	#data_load_state.text('Loading data... done!')
 
-st.subheader('Raw data')
-st.write(data.tail())
+	st.subheader('Raw data')
+	st.write(data.tail())
 
-# Plot raw data
-def plot_raw_data():
+	# Plot raw data
+	def plot_raw_data():
 	fig = go.Figure()
 	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
 	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
 	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
 	st.plotly_chart(fig)
 	
-plot_raw_data()
+	plot_raw_data()
 
-# Predict forecast with Prophet.
-df_train = data[['Date','Close']]
-df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+	# Predict forecast with Prophet.
+	df_train = data[['Date','Close']]
+	df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
 
-m = Prophet()
-m.fit(df_train)
-future = m.make_future_dataframe(periods=period)
-forecast = m.predict(future)
+	m = Prophet()
+	m.fit(df_train)
+	future = m.make_future_dataframe(periods=period)
+	forecast = m.predict(future)
 
-# Show and plot forecast
-st.subheader('Forecast data')
-st.write(forecast.tail())
+	# Show and plot forecast
+	st.subheader('Forecast data')
+	st.write(forecast.tail())
     
-st.write(f'Forecast plot for {n_years} years')
-fig1 = plot_plotly(m, forecast)
-st.plotly_chart(fig1)
+	st.write(f'Forecast plot for {n_years} years')
+	fig1 = plot_plotly(m, forecast)
+	st.plotly_chart(fig1)
 
-st.write("Forecast components")
-fig2 = m.plot_components(forecast)
-st.write(fig2)
+	st.write("Forecast components")
+	fig2 = m.plot_components(forecast)
+	st.write(fig2)
 
-#part2
+
 
 
 
