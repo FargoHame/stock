01b8c869660ee5df27d1e3f6eb9995104452a6ca
braxtonova <85130590+braxtonova@users.noreply.github.com>
2021-06-18 13:19:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -144,14 +144,14 @@ def pre_dict():
     st.write(data.tail())
 
     # Plot raw data
-    def plot_raw_data():
-            fig = go.Figure()
-	    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name = "stock_open"))
-	    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name = "stock_close"))
-	    fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
-	    st.plotly_chart(fig)
+    def runpls():
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name = "stock_open"))
+        fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name = "stock_close"))
+        fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+        st.plotly_chart(fig)
 	
-    plot_raw_data()
+    runpls()
 
     # Predict forecast with Prophet.
     df_train = data[['Date','Close']]
